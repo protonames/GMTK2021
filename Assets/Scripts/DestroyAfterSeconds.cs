3a88d793ad3710e8b5039b6309a36ce63a916750
Felipe Nunes <felipe@protonam.es>
2021-06-11 20:18:34 +0100

    Initial Commit

---
+++ Assets/Scripts/DestroyAfterSeconds.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PNTemplate._Temp
+{
+	public class DestroyAfterSeconds : MonoBehaviour
+	{
+		[SerializeField]
+		private float seconds = 1f;
+
+		public float Seconds
+		{
+			get => seconds;
+			set => seconds = value;
+		}
+
+		private void Start()
+		{
+			Destroy(gameObject, Seconds);
+		}
+	}
+}
